b7c1548751548966ccadd097234b1e885846412f
cdumont <christophe.dumont@noviat.com>
2018-09-07 00:36:49 +0200

    add many snippet + add autocompletion

--- fileTemplates/Odoo Class.py
+++ fileTemplates/Odoo Class.py
@@ -1,5 +1,8 @@
-import logging
+# -*- coding: utf-8 -*-
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
+import logging
 from openerp import models, fields
 
 _logger = logging.getLogger(__name__)
